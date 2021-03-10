54fd109e7fdb7f563240df0098cb59e854c20a86
Dmitry Bakalov <dmitry.a.bakalov@gmail.com>
2021-03-10 08:57:58 -0500

    Capstone Project Solution created and connected to database

---
+++ CapstoneBillCalendar/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneBillCalendar
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CapstoneBillCalendar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CapstoneBillCalendar.MvcApplication" Language="C#" %>
