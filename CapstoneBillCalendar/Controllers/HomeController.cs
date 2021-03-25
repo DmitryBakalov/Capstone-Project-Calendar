f907da98d6229f1a8145ab3f496b8d57539d18fd
Dmitry Bakalov <dmitry.a.bakalov@gmail.com>
2021-03-25 00:04:11 -0400

    Added AddEvent and DeleteEvent properties

--- CapstoneBillCalendar/Controllers/HomeController.cs
+++ CapstoneBillCalendar/Controllers/HomeController.cs
@@ -39,5 +39,51 @@ namespace CapstoneBillCalendar.Controllers
                 };
             }
         }
+
+        [HttpPost]
+        public JsonResult SaveEvent(BillPayment e)
+        {
+            var status = false;
+            using (BillCalendarDatabaseEntities dc = new BillCalendarDatabaseEntities())
+            {
+                if (e.PaymentId > 0)
+                {
+                    // Update the event
+                    var v = dc.BillPayments.Where(a => a.PaymentId == e.PaymentId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.PayeeName = e.PayeeName;
+                        v.DueDate = e.DueDate;
+                        v.PaidDate = e.PaidDate;
+                        v.Description = e.Description;
+                        v.Paid = e.Paid;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.BillPayments.Add(e);
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int paymentID)
+        {
+            var status = false;
+            using (BillCalendarDatabaseEntities dc = new BillCalendarDatabaseEntities())
+            {
+                var v = dc.BillPayments.Where(a => a.PaymentId == paymentID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.BillPayments.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
+        
 }
